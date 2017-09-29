efd40dfaa3e6838043d873f38e64dc1b2d1985db
Jukka Koivunen <jjkoivun@gmail.com>
2017-09-29 15:11:35 +0200

    LILY-2611: Added migrations for changing from CommaSeparatedIntegerField to CharField.

--- lily/messaging/email/models/models.py
+++ lily/messaging/email/models/models.py
@@ -485,15 +485,13 @@ class EmailOutboxMessage(TenantMixin, models.Model):
     cc = models.TextField(null=True, blank=True, verbose_name=_('cc'))
     headers = models.TextField(null=True, blank=True, verbose_name=_('email headers'))
     mapped_attachments = models.IntegerField(verbose_name=_('number of mapped attachments'))
-    original_attachment_ids = models.CommaSeparatedIntegerField(max_length=255, default='')
-    original_attachment_ids_char = models.CharField(max_length=255,
+    original_attachment_ids = models.CharField(max_length=255,
                                                     default='',
                                                     validators=[validate_comma_separated_integer_list]
                                                     )
     subject = models.CharField(null=True, blank=True, max_length=255, verbose_name=_('subject'))
     send_from = models.ForeignKey(EmailAccount, verbose_name=_('from'), related_name='outbox_messages')
-    template_attachment_ids = models.CommaSeparatedIntegerField(max_length=255, default='')
-    template_attachment_ids_char = models.CharField(max_length=255,
+    template_attachment_ids = models.CharField(max_length=255,
                                                     default='',
                                                     validators=[validate_comma_separated_integer_list]
                                                     )
